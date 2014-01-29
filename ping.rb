a9783cd33ec75a42eb2441f91c833e092a193c49
Randy Noah <randy@fluffy.homeip.net>
2014-01-29 17:17:27 -0500

    Works with linux ping

--- ping.rb
+++ ping.rb
@@ -2,14 +2,14 @@ require 'time'
 
 ping_count = 1
 server = "www.google.com"
-outfile = "pinglog.txt"
+outfile = "/home/randy/rails_projects/ping_app/pinglog.txt"
 
 def pingx(server="www.google.com",  count=1)
-    result = `ping -n #{count} #{server}`
+    result = `ping -c #{count} #{server}`
 
-    mtch = result.match(/Reply .* TTL=[\d]+/)
+    mtch = result.match(/from .* ttl=[\d]+/)
     time = result.match(/time=[\d]*/)
-    return mtch[0]
+    return mtch[0] + ' ' + time[0]
 end
 
 t = Time.now
